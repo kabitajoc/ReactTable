60c0490e63bcb38dc36b029cd61e1866101a1b19
Kabita Joshi <kabitajoshi49@gmail.com>
2023-09-12 16:09:37 +0545

    footer added to column

--- tanstack_table-v7/src/components/Columns.tsx
+++ tanstack_table-v7/src/components/Columns.tsx
@@ -1,28 +1,33 @@
 import { Column } from "react-table";
-export const COLUMNS : Column[] = [
+export const COLUMNS: Column[] = [
   {
     Header: "Id",
-    
+    Footer: "Id",
     accessor: "id",
   },
   {
     Header: "First Name",
+    Footer: "First Name",
     accessor: "first_name",
   },
   {
     Header: "Last Name",
+    Footer: "Last Name",
     accessor: "last_name",
   },
   {
     Header: "Date Of Birth",
+    Footer: "Date Of Birth",
     accessor: "date_of_birth",
   },
   {
     Header: "Country",
+    Footer: "Country",
     accessor: "country",
   },
   {
     Header: "Phone",
+    Footer: "Phone",
     accessor: "phone",
   },
 ];
